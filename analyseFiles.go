ad0b89015663b3b7a55f1a421fff3aa0a0d2a223
William Jacobsen <villi05.v.j@gmail.com>
2025-07-20 13:25:05 +0200

    clean up

--- analyseFiles.go
+++ analyseFiles.go
@@ -1,6 +1,29 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
+func analyseFiles(fileUrls []string, fileExtensions []string) {
+	fileLines := map[string]int{}
+
+	for _, url := range fileUrls {
+		if slices.Contains(fileExtensions, getFileExtensionFromUrl(url)) {
+			fileLines[url] = amountOfLinesInFile(url)
+
+			if LOGGING {
+				fmt.Println(url)
+				fmt.Printf("Amount of lines in file: %d\n\n", fileLines[url])
+			}
+		}
+	}
+
+	if LOGGING {
+		fmt.Printf("Total amount of lines written: %d\n", totalAmountOfLines(fileLines))
+	}
+}
 
 func amountOfLinesInFile(url string) int {
 	code := getHtml(url)
--- main.go
+++ main.go
@@ -1,29 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"slices"
-)
-
 const (
 	LOGGING = true
+	SAVE_RESULTS_TO_FILE = true
 )
 
 func main() {
 	fileUrls := getAllFileUrls()
 	fileExtensions := getFileExtensions()
 
-	fileLines := map[string]int{}
-
-	for _, url := range fileUrls {
-		if slices.Contains(fileExtensions, getFileExtensionFromUrl(url)) {
-			fileLines[url] = amountOfLinesInFile(url)
-
-			fmt.Println(url)
-			fmt.Printf("Amount of lines in file: %d\n\n", fileLines[url])
-
-		}
-	}
-
-	fmt.Printf("Total amount of lines written: %d\n", totalAmountOfLines(fileLines))
+	analyseFiles(fileUrls, fileExtensions)
 }
