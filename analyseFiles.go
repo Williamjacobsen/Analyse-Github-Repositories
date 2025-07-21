2939b1fb8a8b35575285a6ca547cbc2d30ec2b60
William Jacobsen <villi05.v.j@gmail.com>
2025-07-21 13:16:09 +0200

    code moved

--- analyseFiles.go
+++ analyseFiles.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
 	"slices"
 	"strings"
 )
@@ -14,7 +11,7 @@ type AnalysisResult struct {
 	TotalLineCount int            `json:"total amount of lines"`
 }
 
-func analyseFiles(fileUrls []string, fileExtensions []string) {
+func analyseFiles(fileUrls []string, fileExtensions []string) AnalysisResult {
 	fileLines := map[string]int{}
 
 	for _, url := range fileUrls {
@@ -28,41 +25,14 @@ func analyseFiles(fileUrls []string, fileExtensions []string) {
 		}
 	}
 
-	var totalLineCount int
+	totalLineCount := totalAmountOfLines(fileLines)
 
-	if LOGGING && SAVE_RESULTS_TO_FILE {
-		totalLineCount = totalAmountOfLines(fileLines)
+	result := AnalysisResult{
+		FileLines:      fileLines,
+		TotalLineCount: totalLineCount,
 	}
 
-	if LOGGING {
-		fmt.Printf("Total amount of lines: %d\n", totalLineCount)
-	}
-
-	if SAVE_RESULTS_TO_FILE {
-		result := AnalysisResult{
-			FileLines:      fileLines,
-			TotalLineCount: totalLineCount,
-		}
-
-		encoder, file := createJsonOutputFile()
-		defer file.Close()
-
-		if err := encoder.Encode(result); err != nil {
-			log.Fatalf("could not encode to json file: %s", err)
-		}
-	}
-}
-
-func createJsonOutputFile() (*json.Encoder, *os.File) {
-	file, err := os.Create("analysis_result.json")
-	if err != nil {
-		log.Fatalf("could not create analysis_result.json: %s", err)
-	}
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-
-	return encoder, file
+	return result 
 }
 
 func amountOfLinesInFile(url string) int {
